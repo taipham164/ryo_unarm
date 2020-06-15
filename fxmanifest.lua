01432746a15f3ae5fb5c2cdb59f643ec1121cc4d
Tai Pham <taipham164@gmail.com>
2020-06-15 10:45:55 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,25 +2,9 @@ fx_version 'adamant'
 
 game 'gta5'
 
-description 'ESX Hospital'
-
-version '1.0.0'
-
-server_scripts {
-  '@mysql-async/lib/MySQL.lua',
-	'@es_extended/locale.lua',
-  'locales/en.lua',
-  'config.lua',
-  'server/main.lua'
-}
 
 client_scripts {
-  '@es_extended/locale.lua',
-  'locales/en.lua',
-  'config.lua',
-  'client/main.lua'
+  'main.lua'
 }
 
-dependency 'es_extended'
-
-client_script "UzmvsGzucRrmTXz.lua"
+dependency 'esx_license'
